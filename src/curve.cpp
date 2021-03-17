b1f50d93b5dbc818cbd640b8ffb9e6bf9e533cac
Dennis Rohde <dennis.rohde-t1b@rub.de>
2021-03-17 18:27:58 +0100

    rework numpy data read-in

--- src/curve.cpp
+++ src/curve.cpp
@@ -8,6 +8,8 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+#include <typeinfo>
+
 #include "curve.hpp"
 #include "simplification.hpp"
 
@@ -24,44 +26,39 @@ Curve::Curve(const Points &points, const std::string &name) : Points(points), vs
 Curve::Curve(const np::ndarray &in, const std::string &name) : Points(in.shape(0), in.get_nd() > 1 ? in.shape(1) : 1), name{name}, vstart{0}, vend{Points::size() - 1} {
     const auto n_dimensions = in.get_nd();
     if (n_dimensions > 2){
-        std::cerr << "A Curve requires a 1- or 2-dimensional numpy array of type double."<< std::endl;
+        std::cerr << "A Curve requires a 1- or 2-dimensional numpy array of type " << typeid(coordinate_t).name() << "." << std::endl;
         std::cerr << "Current dimensions: " << n_dimensions << std::endl;
-        std::cerr << "Current type: " << p::extract<char const*>(p::str(in.get_dtype())) << std::endl;
+        std::cerr << "Current type: " << p::extract<const char *>(p::str(in.get_dtype())) << std::endl;
         std::cerr << "WARNING: constructed empty curve" << std::endl;
         return;
     }
     if (in.get_dtype() != np::dtype::get_builtin<coordinate_t>()) {
-        std::cerr << "A Polygonal_Curve requires a 1- or 2-dimensional numpy array of type double."<< std::endl;
+        std::cerr << "A Curve requires a 1- or 2-dimensional numpy array of type " << typeid(coordinate_t).name() << "." << std::endl;
         std::cerr << "Current dimensions: " << n_dimensions << std::endl;
-        std::cerr << "Current type: " << p::extract<char const*>(p::str(in.get_dtype())) << std::endl;
+        std::cerr << "Current type: " << p::extract<const char *>(p::str(in.get_dtype())) << std::endl;
         std::cerr << "WARNING: constructed empty curve" << std::endl;
         return;
     }
     const auto number_points = in.shape(0);
-    const auto strides0 = in.strides(0) / sizeof(coordinate_t);
-    
-    const auto data = reinterpret_cast<const coordinate_t*>(in.get_data());
-    
+    const auto strides = in.get_strides();
+        
     if (n_dimensions == 2) {
         const auto point_size = in.shape(1);
-        const auto strides1 = in.strides(1) / sizeof(coordinate_t);
         
         #if DEBUG
         std::cout << "constructing curve of size " << number_points << " and " << point_size << " dimensions" << std::endl;
         #endif
                 
         #pragma omp parallel for simd
-        for (curve_size_t i = 0; i < number_points; ++i) {            
-            auto coord_data = data + i * strides0;
-            
-            for(curve_size_t j = 0; j < point_size; ++j, coord_data += strides1){
-              Points::operator[](i)[j] = *coord_data;
+        for (curve_size_t i = 0; i < number_points; ++i) {                        
+            for(curve_size_t j = 0; j < point_size; ++j){
+              Points::operator[](i)[j] = *reinterpret_cast<const coordinate_t *>(in.get_data() + i * strides[0] + j * strides[1]);
             }
         }
     } else {                
         #pragma omp parallel for simd
         for (curve_size_t i = 0; i < number_points; ++i) {
-            Points::operator[](i)[0] = *(data + i * strides0);
+            Points::operator[](i)[0] = *reinterpret_cast<const coordinate_t *>(in.get_data() + i * strides[0]);
         }
     }
     
